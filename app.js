48af79b737379d271536dca09ee304849bf25970
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-02-20 12:26:52 +0530

    routes added

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-import express from 'express';
-import { PORT } from './config/env.js';
-
+import express from "express";
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.route.js";
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Welcome!!!')
+
+//utilising the routes
+app.use("/api/v1/auth", authRouter); //pre-pending /api/v1/auth before each authRoute mentioned inside authRouter
+app.use("api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to subscription tracker API!!!");
 });
 
-app.listen(PORT ,() => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({title: 'GET all users'}));
-userRouter.get('/:id', (req, res) => res.send({title: 'GET user details'}));
-userRouter.post('/', (req, res) => res.send({title: 'CREATE new users'})); //same routes can have differnt methods
-userRouter.put('/:id', (req, res) => res.send({title: 'UPDATE user'}));
-userRouter.delete('/:id', (req, res) => res.send({title: 'DELETE user'}));
+userRouter.get("/", (req, res) => res.send({ title: "GET all users" }));
+userRouter.get("/:id", (req, res) => res.send({ title: "GET user details" }));
+userRouter.post("/", (req, res) => res.send({ title: "CREATE new users" })); //same routes can have differnt methods
+userRouter.put("/:id", (req, res) => res.send({ title: "UPDATE user" }));
+userRouter.delete("/:id", (req, res) => res.send({ title: "DELETE user" }));
 
 export default userRouter;
