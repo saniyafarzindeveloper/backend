2e64a2dacae819f123e5c17788d2cb8d54a12c80
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-02-26 20:29:13 +0530

    consumed error middlewares

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.route.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json()); //inbuilt error handling
+app.use(express.urlencoded({ extended: false })); //this helps in understanding form data sent by HTML in simple format
+app.use(cookieParser()); //reads cookie data
 
 //utilising the routes
 app.use("/api/v1/auth", authRouter); //pre-pending /api/v1/auth before each authRoute mentioned inside authRouter
 app.use("api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
+//utilising error middleware
+app.use(errorMiddleware);
+
 app.get("/", (req, res) => {
   res.send("Welcome to subscription tracker API!!!");
 });
 
-app.listen(PORT, async() => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   await connectToDatabase();
 });
--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,26 +1,38 @@
 const errorMiddleware = (err, req, res, next) => {
   try {
-    let error = {...err}
+    let error = { ...err };
     error.message = err.message;
     console.error(err);
 
     //figuring out the type of error
     //eg 1 - mongoose bad ID
-    if(err.name === "CastError"){
-        const message = 'Resource not found';
-        error = new Error(message);
-        error.statusCode = 404;
+    if (err.name === "CastError") {
+      const message = "Resource not found";
+      error = new Error(message);
+      error.statusCode = 404;
     }
 
     //eg 2 - mongoose duplicate key error
-    if(err.code === 11000){
-        const message = 'Duplicate field value entered';
-        error = new Error(message);
-        error.statusCode = 400;
+    if (err.code === 11000) {
+      const message = "Duplicate field value entered";
+      error = new Error(message);
+      error.statusCode = 400;
     }
+
+    //eg 3 - mongoose validation error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((val) => val.message); //mapping because there might be more than 1 validation error
+      error = new Error(message.join(", ")); //separate by ,
+      error.statusCode = 400;
+    }
+
+    res.status(error.statusCode || 500).json({
+      success: false,
+      error: error.message || "Server Error",
+    });
   } catch (error) {
     next(error);
   }
 };
 
-export default errorMiddleware
+export default errorMiddleware;
